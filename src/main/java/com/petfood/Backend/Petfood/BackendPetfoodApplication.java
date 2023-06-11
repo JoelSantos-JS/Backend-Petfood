a0977bca0a35abbd93e0611143f424397537a7c9
Joel Santos <joeltere9@gmail.com>
2023-06-11 16:20:02 -0300

    firts commit

---
+++ src/main/java/com/petfood/Backend/Petfood/BackendPetfoodApplication.java
@@ -0,0 +1,13 @@
+package com.petfood.Backend.Petfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPetfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPetfoodApplication.class, args);
+	}
+
+}
