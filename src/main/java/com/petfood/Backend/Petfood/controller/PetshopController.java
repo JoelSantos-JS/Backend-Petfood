b64c1dfb800836f534ce6682b2ff9f2a1e374e1b
Joel Santos <joeltere9@gmail.com>
2023-06-12 13:17:48 -0300

    Controller petshop

--- src/main/java/com/petfood/Backend/Petfood/controller/PetshopController.java
+++ src/main/java/com/petfood/Backend/Petfood/controller/PetshopController.java
@@ -3,9 +3,17 @@ package com.petfood.Backend.Petfood.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.petfood.Backend.Petfood.dto.PetshopDTO;
 import com.petfood.Backend.Petfood.services.PetshopService;
 
 @RestController
@@ -14,5 +22,39 @@ public class PetshopController {
 
     @Autowired
     private PetshopService petshopService;
+
+    @GetMapping
+    public ResponseEntity<Page<PetshopDTO>> getAllPetshops(Pageable pageable) {
+        Page<PetshopDTO> list = petshopService.findAll(pageable);
+        return ResponseEntity.ok().body(list);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PetshopDTO> findById(@PathVariable long id) {
+        PetshopDTO Petshop = petshopService.findById(id);
+        return ResponseEntity.ok().body(Petshop);
+    }
+
+    @PostMapping
+    public ResponseEntity<PetshopDTO> createPetshop(@RequestBody PetshopDTO PetshopDTO) {
+        PetshopDTO Petshop = petshopService.insert(PetshopDTO);
+        return ResponseEntity.ok().body(Petshop);
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PetshopDTO> updatePetshop(@PathVariable long id, @RequestBody PetshopDTO PetshopDTO) {
+        PetshopDTO PetshopDTO2 = petshopService.update(id, PetshopDTO);
+
+        return ResponseEntity.ok().body(PetshopDTO2);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deletePetshop(@PathVariable long id) {
+        petshopService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
 
 }
